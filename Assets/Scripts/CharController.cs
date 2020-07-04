0cce47487cf1c09a3197dd4df2d058684eb3c814
Daniel George <danieljamesgeorge@gmail.com>
2020-07-04 21:12:46 +0100

    first commit

---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    
+    private bool facingRight = true;
+    public float amountToAccelerate;
+
+    void changeDirection()
+    {
+        transform.Rotate(0,0,180f);
+        facingRight = !facingRight;
+    }
+
+    int leftOrRght()
+    {
+        InputHandling handleInput= GameObject.Find("GameManager").GetComponent<InputHandling>();
+        string LorR = handleInput.processInput();
+        if (LorR == "Right")
+        {
+            return 1;
+        }
+        else if (LorR == "Left")
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        };
+    }
+
+    void addVelo()
+    {
+
+        GetComponent<Rigidbody>().velocity += new Vector3(amountToAccelerate * Time.deltaTime * leftOrRght(), 0, 0);
+    }
+
+    void triggerChangeDirection(){
+        if((facingRight && GetComponent<Rigidbody>().velocity.x<0)||(!facingRight && GetComponent<Rigidbody>().velocity.x>0)){
+            changeDirection();
+        }
+    }
+
+
+
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addVelo();
+        triggerChangeDirection();
+        Debug.Log(GetComponent<Rigidbody>().velocity.x);
+    }
+}
---
+++ Assets/Scripts/InputHandling.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandling : MonoBehaviour
+{
+    
+    public void identifyInput()
+    {
+        if (Input.anyKeyDown)
+        {
+
+            processInput();
+
+        }
+
+
+    }
+
+    public string processInput()
+    {
+
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            return "Left";
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            return "Right";
+        }
+        else if (Input.GetKey(KeyCode.Space)){
+            return "Jump";
+        }
+        else if (Input.GetKey(KeyCode.P)){
+            return "Pause";
+        }
+        else if (Input.GetKey(KeyCode.KeypadEnter)){
+            return "Enter";
+        }
+        else{
+            return "Not Acknowledged Input";
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        identifyInput();
+
+
+    }
+}
